4c8eb366017ac1de67a3539ac9e228e0291ae7d5
sushantk-mhty <sushant.mhty@outlook.com>
2023-09-21 20:41:37 +0530

    add loader

--- studentadminportal-ui/src/app/app.module.ts
+++ studentadminportal-ui/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { TopNavComponent } from './layout/components/top-nav/top-nav.component';
 import { StudentsComponent } from './components/students/students.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { NgxUiLoaderHttpModule, NgxUiLoaderModule } from 'ngx-ui-loader';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,38 @@ import { FormsModule } from '@angular/forms';
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    HttpClientModule
+    HttpClientModule,
+    NgxUiLoaderModule.forRoot({  
+      "bgsColor": "red",
+      "bgsOpacity": 0.5,
+      "bgsPosition": "bottom-right",
+      "bgsSize": 60,
+      "bgsType": "ball-spin-clockwise",
+      "blur": 5,
+      "delay": 0,
+      "fastFadeOut": true,
+      "fgsColor": "#ffffff",
+      "fgsPosition": "center-center",
+      "fgsSize": 60,
+      "fgsType": "three-strings",
+      "gap": 24,
+      "logoPosition": "center-center",
+      "logoSize": 120,
+      "logoUrl": "",
+      "masterLoaderId": "master",
+      "overlayBorderRadius": "0",
+      "overlayColor": "rgba(40, 40, 40, 0.8)",
+      "pbColor": "red",
+      "pbDirection": "ltr",
+      "pbThickness": 3,
+      "hasProgressBar": false,
+      "text": "Loading...",
+      "textColor": "#FFFFFF",
+      "textPosition": "center-center",
+      "maxTime": -1,
+      "minTime": 300
+    }),
+      NgxUiLoaderHttpModule.forRoot({ showForeground: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
